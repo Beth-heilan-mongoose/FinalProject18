197e5f1d4dded24c81d04acd0be4e418f723ac8b
Bethheilanmongoose <bparrillo@ucvts.org>
2019-01-03 15:41:15 +0000

    First-Day-Characters

---
+++ TrappedInTheLaundromat.py
@@ -0,0 +1,32 @@
+import random
+
+print("Just when you thought the worst was over with your nightmare in the dryer, the worst is yet to come. For so begins the long, treacherous journey of trying to escape the terrors of the not-so-friendly neighborhood laundromat. Best of luck. The crocodiles bite.")
+
+#create a character
+class Player:
+    """Person whom you will be playing as"""
+def__init__(self, name, description, health, attack, defense, weapon):
+    self.name = name
+    self.description = overview
+    self.health = hp
+    self.attack = atk
+    self.defense = defense
+    self.weapon = arms
+
+character_list = {
+    'champions': {
+        ('Wallace', 'Julia', 'Franc', 'Jack', 'Ribeye','Lamb Chop')
+    }
+}
+
+wallace = Player("Wallace","An unapologetically Scottish lumberjack who has many pairs of overalls that must be cleaned thoroughly. Armed with a passion for clean dungarees, this monsterous man will do anything to escape his treacherous laundry run.", 212, 24, 26, "wire hangers"
+julia = Player("Julia", "A mild-mannered high-school girl making a living from tutoring a little creature. Is secretly a slayer of men.", 170, 20, 19, "soap flakes w/ 5% concentrate of baking soda"
+franc = Player("Franc", "A sensitive soul whose cuff links are made of pig teeth, one from each of his vanquished porkers in his days as a pig wrestler.", 194, 28, 15, "belt made of sausage links from his pig-wrestling days"
+jack = Player("Jack", "A CDM who takes no prisoners in his tackles. Only scores from volleys. Washes clothes by hand.", 188, 26, 35, "pet grolar bear that has ferocious teeth"
+ribeye = Player("Ribeye", "A product of the Tasmanian outback who rather resembles an echidna at the best of times and the devil at the worst of times.", 175, 23, 18, "soaked towels twisted into lariats"
+lamb chop = Player("Lamb Chop", "A creature of dubious means who is well done...", 200, 17, 22, "an army of weasels who hearken to his every word"
+
+
+
+
+
