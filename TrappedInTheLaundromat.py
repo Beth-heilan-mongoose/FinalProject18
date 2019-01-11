147a398f92abf448074d970f6225e92d5645cfe8
Bethheilanmongoose <bparrillo@ucvts.org>
2019-01-11 15:48:51 +0000

    Modifiy

--- TrappedInTheLaundromat.py
+++ TrappedInTheLaundromat.py
@@ -6,11 +6,11 @@ class Player:
     """Person whom you will be playing as"""
     def __init__(self, name, description, health, attack, defense, weapon):
         self.name = name
-        self.description = overview
-        self.health = hp
-        self.attack = atk
+        self.description = description
+        self.health = health
+        self.attack = attack
         self.defense = defense
-        self.weapon = arms
+        self.weapon = weapon
 
     def introduce(self):
         print(f"""{self.name} Character Profile:
@@ -18,7 +18,7 @@ class Player:
 - Health - {self.health}
 - Attack - {self.attack}
 - Defense - {self.defense}
-- Weapon - {self.weapon}
+- Weapon - {self.weapon}""")
 
 character_list = {
     'champions': {
@@ -31,29 +31,29 @@ julia = Player("Julia", "A mild-mannered high-school girl making a living from t
 franc = Player("Franc", "A sensitive soul whose cuff links are made of pig teeth, one from each of his vanquished porkers in his days as a pig wrestler.", 194, 28, 15, "belt made of sausage links from his pig-wrestling days")
 jack = Player("Jack", "A CDM who takes no prisoners in his tackles. Only scores from volleys. Washes clothes by hand.", 188, 26, 35, "pet grolar bear that has ferocious teeth")
 ribeye = Player("Ribeye", "A product of the Tasmanian outback who rather resembles an echidna at the best of times and the devil at the worst of times.", 175, 23, 18, "soaked towels twisted into lariats")
-lamb chop = Player("Lamb Chop", "A creature of dubious means who is well done...", 200, 17, 22, "an army of weasels who hearken to his every word")
+lamb_chop = Player("Lamb Chop", "A creature of dubious means who is well done...", 200, 17, 22, "an army of weasels who hearken to his every word")
 
 choice = input("Choose a character.")
-    if choice == wallace:
-        active_character = wallace
-    elif choice == julia:
-        active character = julia
-    elif choice == franc:
-        active character = franc
-    elif choice == jack:
-        active character = jack
-    elif choice == ribeye:
-        active character = ribeye
-    else:
-        active character = lamb chop
+if choice == wallace:
+    active_character = wallace
+elif choice == julia:
+    active_character = julia
+elif choice == franc:
+    active_character = franc
+elif choice == jack:
+    active_character = jack
+elif choice == ribeye:
+    active_character = ribeye
+else:
+    active_character = lamb_chop
 
-play = input
+play = input("Would you like to enter the magical world of the laundromat? Type 'yes' or 'no'.")
 if play.lower() == "yes":
     def scene_1():
         print("Introduction:")
         print("Just when you thought the worst was over with your nightmare in the dryer, the worst is yet to come. For so begins the long, treacherous journey of trying to escape the terrors of the not-so-friendly neighborhood laundromat. Best of luck. The crocodiles bite.")
         choice = input("Choose a character:")
-        print character_list
+        print({character_list})
         x()
         input("Type the name of the character you wish to escape the labyrinth of the laundromat with.")
         x()
@@ -76,3 +76,15 @@ if play.lower() == "yes":
         print("You must decide which tiles are safe. The tiles on the floor are black, grey, and white, but all three colors of tile are collapsing. There must be a sequence. Choose a color tile.")
         x()
         input("Choose a color tile to step on. Type b for black, g for grey, and w for white.")
+        while tiles_left:
+            if input == 'b':
+                print("You step hesitantly onto the tile. It holds. You breathe a sign of relief... and then it collapses. You were almost there. Tough luck.")
+            elif input == 'g':
+                print("You confidently jump onto the tile, which promptly collapses. Game over.")
+            elif input != 'b','g','w':
+                print("There are only three tile colors. Maybe you're feeling a bit weird after that ordeal in the dryer? Take a minute and try again.")
+            else:
+                print("The tile holds. Now you must choose the next tile.")
+
+
+
