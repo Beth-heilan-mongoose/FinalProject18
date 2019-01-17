5b4279027f616e9513761ab505a2f76907fde7ec
Bethheilanmongoose <bparrillo@ucvts.org>
2019-01-17 15:21:27 +0000

    Crikey

--- TrappedInTheLaundromat.py
+++ TrappedInTheLaundromat.py
@@ -53,123 +53,109 @@ has_crocodile == false
 def game():
 
     play = input("Would you like to enter the magical world of the laundromat? Type 'yes' or 'no'.")
-if play.lower() == "yes":
-    def scene_1():
-        print("Introduction:")
-        print("Just when you thought the worst was over with your nightmare in the dryer, the worst is yet to come. For so begins the long, treacherous journey of trying to escape the terrors of the not-so-friendly neighborhood laundromat. Best of luck. The crocodiles bite.")
-        choice = input("Choose a character:")
-        print({character_list})
-        x()
-        input("Type the name of the character you wish to escape the labyrinth of the laundromat with.")
-        x()
-        print("[active_character], your mission is clear. Escape the atrocities of the laundromat.")
-        x()
-        print("You step out of the dryer, a little fatigued but eager to get out of the laundromat forever. You will NOT be coming back.")
-        x()
-        print("Collecting your laundry, you turn around, only to see that the entire room has sunken into the ground. You must climb the levels in order to escape.")
-        x()
-        print("Gripping your basket and your {self.weapon}, you begin across the room.")
-        x()
-        print("The first level passes without event, and you begin to think to yourself that this is going to be easy. Some earthquake must have sunken the floor while you had your episode in the dryer, but everything's okay now.")
-        x()
-        print("You are quite wrong.For just as you lay your foot upon the second level, the tile under it collapses, causing you to trip.")
-        x()
-        print("You lay on the ground as tiles collapse around and under you, stunned. And to think that this was just a weekly visit to the laundromat!")
-        x()
-        print("Your basket of laundry falls through the floor as the tiles around it collapse. But that is not your priority right now. It's life or death, and the last thing on your mind is whether your Gucci shirts will ever be seen again.")
-        x()
-        print("You must decide which tiles are safe. The tiles on the floor are black, grey, and white, but all three colors of tile are collapsing. There must be a sequence. Choose a color tile.")
-        x()
-        input_1 = input("Choose a color tile to step on. Type b for black, g for grey, and w for white.")
-        tiles_left = 4
-        while tiles_left:
-            if input_1 == 'b':
-                print("You step hesitantly onto the tile. It holds. You breathe a sign of relief... and then it collapses. You were almost there. Tough luck.")
-            elif input_1 == 'g':
-                print("You confidently jump onto the tile, which promptly collapses. Game over.")
-            elif input_1 != ('b','g','w'):
-                print("There are only three tile colors. Maybe you're feeling a bit weird after that ordeal in the dryer? Take a minute and try again.")
-            else:
-                print("The tile holds. Now you must choose the next tile.")
-        x()
-        print("You stumble across the rest of the tiles. Congratulations! You have passed the second level!")
-        x()
-        print("The third level is full of shattered washing machines and dryers. You warily glance around, and are shocked to see some wild creatures crawling out of the debris and broken pieces.")
-        x()
-        print("Among other horrendous beasts, a few crocodiles, snakes, and large tortoises appear out of the wreckage. You freak out.")
-        x()
-        print("                     '``````^~"-,_`"-,_
-        print("       .-~c~-.                    `~:. ^-.
-        print("    `~~~-.c    ;                      `:.  `-,     _.-~~^^~:.
-        print("          `.   ;      _,--~~~~-._       `:.   ~. .~          `.
-        print("         .` ;'   .:`           `:       `:.   `    _.:-,.    `.
-        print("        .' .:   :'    _.-~^~-.    `.       `..'   .:      `.    '
-        print("        :  .' _:'   .-'        `.    :.     .:   .'`.        :    ;
-        print("        :  `-'   .:'             `.    `^~~^`   .:.  `.      ;    ;
-        print("        `-.__,-~                  ~-.        ,' ':    '.__.`    :'
-        print("                                     ~--..--'     ':.         .:'
-        print("                                                     ':..___.:'
-        x()
-                    _.--....
-                 _....---;:'::' ^__/
-               .' `'`___....---=-'`
-              /::' (`
-              \'   `:.
-               `\::.  ';-"":::-._  {}
-            _.--'`\:' .'`-.`'`.' `{I}
-         .-' `' .;;`\::.   '. _: {-I}`\
-       .'  .:.  `:: _):::  _;' `{=I}.:|
-      /.  ::::`":::` ':'.-'`':. {_I}::/
-      |:. ':'  :::::  .':'`:. `'|':|:'
-       \:   .:. ''' .:| .:, _:./':.|
-        '--.:::...---'\:'.:`':`':./
-                       '-::..:::-'
-        (x)
-        input_2 = input("You are deathly afraid of... a) Rodents of Unusual Size b) Gila Monsters c) Bats (thanks Mr. Stanko)")
-        if input_2 == a:
-            print("Just your luck. It would be against all copyright laws to put those in this game. Proceed.")
-        elif input_2 == b:
-            print("HAHA! Not only are Gila Monsters frequent customers of laundromats, they just happen to be in this one! Overcome thy fear or perish!")
-        elif input_2 == c:
-            print("Hurry and see a healthcare provider. Being in the same room as these bats, no matter what kind, is extremely dangerous. Their rabies may spread. That's a important note, and will be even more important IF you get out.")
-        x()
-        print("You crawl on top of the nearest washing machine. YOU HAVE THE HIGH GROUND NOW.")
-        x()
-        print("You draw your {weapon} and prepare to use it. The crocodiles snap at you. They are vicious creatures, and very hungry.")
-        x()
-        print("              _.'^^'.    ")
-        print(" _        _.-' ((@)) '.   ./\/\/\/\/\/\,.---.__ ")
-        print(" ..'o'...-'      ~~~    '~/\/\/\/\/\/\__.---.   `-._ ")
-        print(":                         /\/\/\/\,-'              `-.__ "")
-        print(""^VvvvvvvvvvvVvVv                  |                     `-._._._ ")
-        print(";^^^^^^^^^^^`         /             `\          /               `-._ ")
-        print("```````````````'.`                    `\        (                 `'-._ ")
-        print("          .-----'`   /\               `\    )--.______.______._______`/ ")
-        print("          (((------'``  `'--------'`(((----' ")
+    if play.lower() == "yes":
+        def scene_1():
+            print("Introduction:")
+            print("Just when you thought the worst was over with your nightmare in the dryer, the worst is yet to come. For so begins the long, treacherous journey of trying to escape the terrors of the not-so-friendly neighborhood laundromat. Best of luck. The crocodiles bite.")
+            choice = input("Choose a character:")
+            print({character_list})
+            x()
+            input("Type the name of the character you wish to escape the labyrinth of the laundromat with.")
+            x()
+            print("[active_character], your mission is clear. Escape the atrocities of the laundromat.")
+            x()
+            print("You step out of the dryer, a little fatigued but eager to get out of the laundromat forever. You will NOT be coming back.")
+            x()
+            print("Collecting your laundry, you turn around, only to see that the entire room has sunken into the ground. You must climb the levels in order to escape.")
+            x()
+            print("Gripping your basket and your {self.weapon}, you begin across the room.")
+            x()
+            print("The first level passes without event, and you begin to think to yourself that this is going to be easy. Some earthquake must have sunken the floor while you had your episode in the dryer, but everything's okay now.")
+            x()
+            print("You are quite wrong.For just as you lay your foot upon the second level, the tile under it collapses, causing you to trip.")
+            x()
+            print("You lay on the ground as tiles collapse around and under you, stunned. And to think that this was just a weekly visit to the laundromat!")
+            x()
+            print("Your basket of laundry falls through the floor as the tiles around it collapse. But that is not your priority right now. It's life or death, and the last thing on your mind is whether your Gucci shirts will ever be seen again.")
+            x()
+            print("You must decide which tiles are safe. The tiles on the floor are black, grey, and white, but all three colors of tile are collapsing. There must be a sequence. Choose a color tile.")
+            x()
+            input_1 = input("Choose a color tile to step on. Type b for black, g for grey, and w for white.")
+            tiles_left = 4
+            while tiles_left:
+                if input_1 == 'b':
+                    print("You step hesitantly onto the tile. It holds. You breathe a sign of relief... and then it collapses. You were almost there. Tough luck.")
+                elif input_1 == 'g':
+                    print("You confidently jump onto the tile, which promptly collapses. Game over.")
+                elif input_1 != ('b','g','w'):
+                    print("There are only three tile colors. Maybe you're feeling a bit weird after that ordeal in the dryer? Take a minute and try again.")
+                else:
+                    print("The tile holds. Now you must choose the next tile.")
+            x()
+            print("You stumble across the rest of the tiles. Congratulations! You have passed the second level!")
+            x()
+            print("The third level is full of shattered washing machines and dryers. You warily glance around, and are shocked to see some wild creatures crawling out of the debris and broken pieces.")
+            x()
+            print("Among other horrendous beasts, a few crocodiles, snakes, and large tortoises appear out of the wreckage. You freak out.")
+            x()
+            print("                     '``````^~\"-_`\"-,_ ")
+            print("       .-~c~-.                    `~:. ^-. ")
+            print("    `~~~-.c    ;                      `:.  `-,     _.-~~^^~:. ")
+            print("          `.   ;      _,--~~~~-._       `:.   ~. .~          `. ")
+            print("         .` ;'   .:`           `:       `:.   `    _.:-,.    `. ")
+            print("        .' .:   :'    _.-~^~-.    `.       `..'   .:      `.    ' ")
+            print("        :  .' _:'   .-'        `.    :.     .:   .'`.        :    ; ")
+            print("        :  `-'   .:'             `.    `^~~^`   .:.  `.      ;    ; ")
+            print("        `-.__,-~                  ~-.        ,' ':    '.__.`    :' ")
+            print("                                     ~--..--'     ':.         .:' ")
+            print("                                                     ':..___.:' ")
+            (x)
+            input_2 = input("You are deathly afraid of... a) Rodents of Unusual Size b) Gila Monsters c) Bats (thanks Mr. Stanko)")
+            if input_2 == a:
+                print("Just your luck. It would be against all copyright laws to put those in this game. Proceed.")
+            elif input_2 == b:
+                print("HAHA! Not only are Gila Monsters frequent customers of laundromats, they just happen to be in this one! Overcome thy fear or perish!")
+            elif input_2 == c:
+                print("Hurry and see a healthcare provider. Being in the same room as these bats, no matter what kind, is extremely dangerous. Their rabies may spread. That's a important note, and will be even more important IF you get out.")
+            x()
+            print("You crawl on top of the nearest washing machine. YOU HAVE THE HIGH GROUND NOW.")
+            x()
+            print("You draw your {weapon} and prepare to use it. The crocodiles snap at you. They are vicious creatures, and very hungry.")
+            x()
+            print("              _.'^^'.    ")
+            print(" _        _.-' ((@)) '.   ./\/\/\/\/\/\,.---.__\ ")
+            print(" ..'o'...-'      ~~~    '~/\/\/\/\/\/\__.---.   `-._ ")
+            print(":                         /\/\/\/\,-'              `-.__\ ")
+            print(""^VvvvvvvvvvvVvVv                  |                     \`-._._._ ")
+            print(";^^^^^^^^^^^`         /             `\          /               `-._ ")
+            print("```````````````'.`                    `\        (                 `'-._ ")
+            print("          .-----'`   /\               `\    )--.______.______._______`/ ")
+            print("          (((------'``  `'--------'`(((----' ")
 
-        x()
-        print("You use your {weapon} in a threatening manner, but the {weapon} is quickly devoured by the hungry crocodiles. So much for that.")
-        x()
-        print("Deciding that you are pretty desperate to get out of this situation, you remember a show you used to watch as a child. What was it called?")
-        x()
-        print("Ah, yes. The Crocodile Hunter featuring the impeccable Steve Irwin. All the crocodile-hunting skills you learned as a child will surely help you now. Now what is it that could help you in this situation?")
-        x()
-        input_3 = input("""Which information will you employ from the legendary Crocodile Hunter in order to employ the crocodile as your own?:
-            a) Shout 'Crikey!' as loud as humanly possible and leap onto the back of the crocodile
-            b) Take out a rotting pig carcass and entice the crocodile to eat, then tie its jaws so it will not bite
-            c) Tame it with the soothing touch that only the Crocodile Hunter could give in hopes of winning its affection""")
-        if input_3 == a:
-            print("The crocodile is momentarily confused, twisting its head to see its familiar friend Steve but once it realizes that you are not he, he opens his jaws wide. Game over.")
-            gamecis.exit()
-        elif input_3 == b:
-            print("The crocodiles see the carcass and remember fondly how Steve used to feed them rotting pig carcasses as well. Driven to tears (real tears), they submit to the pig and allow themselves to be muzzled. You have collected a crocodile.")
-            has_crocodile == true
-        elif input_3 == c:
-            print("The crocodiles roll over once you rub their stomachs. Taking advantage of the opportunity, you run away.")
-        x()
-        print("You rejoice in your good luck as you pass to the third level.")
-        x()
-        print("
+            x()
+            print("You use your {weapon} in a threatening manner, but the {weapon} is quickly devoured by the hungry crocodiles. So much for that.")
+            x()
+            print("Deciding that you are pretty desperate to get out of this situation, you remember a show you used to watch as a child. What was it called?")
+            x()
+            print("Ah, yes. The Crocodile Hunter featuring the impeccable Steve Irwin. All the crocodile-hunting skills you learned as a child will surely help you now. Now what is it that could help you in this situation?")
+            x()
+            input_3 = input("""Which information will you employ from the legendary Crocodile Hunter in order to employ the crocodile as your own?:
+                a) Shout 'Crikey!' as loud as humanly possible and leap onto the back of the crocodile
+                b) Take out a rotting pig carcass and entice the crocodile to eat, then tie its jaws so it will not bite
+                c) Tame it with the soothing touch that only the Crocodile Hunter could give in hopes of winning its affection""")
+            if input_3 == a:
+                print("The crocodile is momentarily confused, twisting its head to see its familiar friend Steve but once it realizes that you are not he, he opens his jaws wide. Game over.")
+                gamecis.exit()
+            elif input_3 == b:
+                print("The crocodiles see the carcass and remember fondly how Steve used to feed them rotting pig carcasses as well. Driven to tears (real tears), they submit to the pig and allow themselves to be muzzled. You have collected a crocodile.")
+                has_crocodile == true
+            elif input_3 == c:
+                print("The crocodiles roll over once you rub their stomachs. Taking advantage of the opportunity, you run away.")
+            x()
+            print("You rejoice in your good luck as you pass to the third level.")
+            x()
+            print("")
+        scene_1()
 
 
 
